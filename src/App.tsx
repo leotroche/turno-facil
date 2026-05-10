913c81f759cc61611488c356cdf90b5cb83e9235
Leonardo Troche <leo.m.troche@gmail.com>
2026-05-10 12:06:09 -0300

    feat: add shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
     <div className="grid min-h-dvh place-items-center">
-      <h1 className="text-4xl">Vite + React</h1>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl">Turno Fácil</h1>
+        <Button onClick={() => console.log('Turno solicitado')}>Sacar turno</Button>
+      </div>
     </div>
   )
 }
