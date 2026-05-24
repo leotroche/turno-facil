431587006104e9211467cb90dd6ef92ff485e654
Leonardo Troche <leo.m.troche@gmail.com>
2026-05-24 18:48:41 -0300

    feat: format date and time in TurnosTable using utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,21 @@
 import { clsx, type ClassValue } from 'clsx'
+import { format, parseISO } from 'date-fns'
+import { es } from 'date-fns/locale'
 import { twMerge } from 'tailwind-merge'
+
+// ------------------------------------------------------------
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatFecha(fecha: string) {
-  const date = new Date(fecha + 'T00:00:00')
+// ------------------------------------------------------------
 
-  return new Intl.DateTimeFormat('es-AR', {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  }).format(date)
+export function formatFecha(fecha: string) {
+  return format(parseISO(fecha), "EEEE, d 'de' MMMM", { locale: es })
 }
+
+// ------------------------------------------------------------
 
 export function formatHora(hora: string) {
   return hora.slice(0, 5)
