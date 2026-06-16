0640616d2334464d2401f002f6fd6365b22cf35f
Daniel Cáceres <dragonaladodera2002@hotmail.com>
2026-06-16 00:19:22 -0300

    Update useTurnosMutations

--- src/hooks/useTurnosMutations.ts
+++ src/hooks/useTurnosMutations.ts
@@ -2,7 +2,7 @@ import { useQueryClient, useMutation } from '@tanstack/react-query'
 
 import type { ReservaFormValues } from '@/schemas/reserva-form-schema'
 import type { TurnoFormValues } from '@/schemas/turno-form-schema'
-import { eliminarTurno, actualizarTurno, crearTurno, crearReserva } from '@/services/turno.service'
+import { eliminarTurno, actualizarTurno, crearTurno, crearReserva, cancelarReserva } from '@/services/turno.service'
 import type { TurnoRowUpdate } from '@/types/types'
 
 export function useTurnosMutations() {
@@ -50,10 +50,31 @@ export function useTurnosMutations() {
 
   // ------------------------------------------------------------
 
+  const cancelar = useMutation({ mutationFn: ( turnoId: string,) => cancelarReserva(turnoId,),
+    onSuccess: () => { queryClient.invalidateQueries({
+          queryKey: [
+            'mis-turnos',
+          ],
+        },
+      )
+
+      queryClient.invalidateQueries(
+        {
+          queryKey: [
+            'turnos',
+          ],
+        },
+      )
+    },
+  })
+
+  // ------------------------------------------------------------
+
   return {
     crear,
     actualizar,
     eliminar,
     reservar,
+    cancelar,
   }
 }
