7a611588b89a660b1e38e7e01aea0706100545d8
Daniel Cáceres <dragonaladodera2002@hotmail.com>
2026-06-16 00:18:54 -0300

    Add useMisTurnos

---
+++ src/hooks/useMisTurnos.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { obtenerMisTurnos } from '../services/turno.service'
+import type { Turno } from '../types/types'
+
+export function useMisTurnos() {
+  const { data, isPending, error } =
+    useQuery<Turno[]>({
+      queryKey: ['mis-turnos'],
+      queryFn: obtenerMisTurnos,
+    })
+
+  return {
+    turnos: data ?? [],
+    isPending,
+    error,
+  }
+}
